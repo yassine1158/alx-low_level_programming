c34a96f2dc9cc78c4726a3f236a231d2aa0e2e90
yassine1158 <yassinechaabane72@gmail.com>
2023-08-02 08:29:41 -0700

    h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,6 +1,6 @@
 
-#ifndef MAIN.H
-#define MAIN.H
+#ifndef MAIN_H
+#define MAIN_H
 
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
@@ -12,4 +12,4 @@ int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 
-#enddif /* MAIN_H */
+#endif /* MAIN_H */
