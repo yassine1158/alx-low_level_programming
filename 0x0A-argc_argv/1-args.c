0dee16cf9c5771272234b78da4b2f03c22b16c59
yassine1158 <yassinechaabane72@gmail.com>
2023-08-04 15:49:40 -0700

    hh

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,7 +6,7 @@
  * Return:0-success, non-zero-fail.
 */
 
-int main(int argc, char *argv[] _attribute_((unused)))
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	printf("%d\n", argc - 1);
 	return (0);
