0175527f62e449343bc82b95461f16e0926b69fc
yassine1158 <yassinechaabane72@gmail.com>
2023-08-04 16:00:01 -0700

    hh

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print the number of arguments passed the program
+ * @argc: int
+ * @argv:list
+ * Return: 0
+*/
+
+int main(int argc, char const *argv[])
+{
+	int i = 0;
+
+	while (arg--)
+	{
+		printf("%s\n",argv[i];
+		i ++;
+	}
+	return (0);
+}
